adbb2e4f295ee22bb0ddf09b200efd3f00ac3cce
ningguan-png <83469333+ningguan-png@users.noreply.github.com>
2022-06-27 18:20:31 +0300

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,17 @@
+let pal = document.querySelector(".palochka")
+let pal1 = document.querySelector("#pal1")
+let pal2 = document.querySelector("#pal2")
+let pal3 = document.querySelector("#pal3")
+let pal4 = document.querySelector("#pal4")
+
+
+
+
+pal1.style.animationDuration = String(Math.random() * 100 / 11 + 1.5) +'s'
+console.log(pal1.style.animationDuration)
+pal2.style.animationDuration = String(Math.random() * 100 / 10 + 1.5) +'s'
+console.log(pal2.style.animationDuration)
+pal3.style.animationDuration = String(Math.random() * 100 / 11 + 1.5) +'s'
+console.log(pal3.style.animationDuration)
+pal4.style.animationDuration = String(Math.random() * 100 / 12 + 1.5) +'s'
+console.log(pal4.style.animationDuration)
